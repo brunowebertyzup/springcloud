89b10fdfd5d4e1a5f28147558b71a044d6768743
brunowebertyzup <82046327+brunowebertyzup@users.noreply.github.com>
2021-07-07 17:00:31 -0300

    Inicio projeto spring cloud

---
+++ src/main/java/br/com/zupacademy/bruno/springcloud/SpringcloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.bruno.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+
+}
